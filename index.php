b88062bfb184210f245c45e61abc327e3df575c9
Jonnuelle Aguilar <jaguilar.global@mphi.com>
2021-01-05 11:29:57 +0800

    Sample Index file has been added.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//-------------------------------------------------------------------------------------------------
+//Program Title   : 
+//Transaction Code: ZMEG_  	 
+//Description     :
+//Specs           :	
+//Date Created    : 01/22/2020
+//Module          :	
+//Functional      : Al Joseph
+//Developer       : Lee Alvin Naeg
+//-------------------------------------------------------------------------------------------------
+//                       Revision History
+//-------------------------------------------------------------------------------------------------
+// Date       | Person Responsible     | Description of Changes               | Ref./Tckt/Trans No.
+// 01/01/9999 |                        |								      |					
+//-------------------------------------------------------------------------------------------------
+
+
+include("Config/Config.php");       	   //Config  
+include("routes.php");                 //Controller
+echo 'sample';
+?>
+
